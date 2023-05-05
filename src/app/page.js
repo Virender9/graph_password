a15db07c9171287f91770e48ea7ccb352b9446e2
Virende9 <r.virenderverma1@gmail.com>
2023-05-05 14:42:20 +0530

    This is college major project

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,304 @@
-import Image from 'next/image'
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 
-export default function Home() {
+const Page = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <>
+      <div className='min-h-full'>
+        <nav className='bg-gray-800'>
+          <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
+            <div className='flex h-16 items-center justify-between'>
+              <div className='flex items-center'>
+                <div className='flex-shrink-0'>
+                  {/* <img className="h-8 w-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company"> */}
+                </div>
+                <div className='hidden md:block'>
+                  <div className='ml-10 flex items-baseline space-x-4'>
+                    {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
+                    <Link
+                      href='#'
+                      className='bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium'
+                      aria-current='page'
+                    >
+                      Dashboard
+                    </Link>
+                    <Link
+                      href='#'
+                      className='text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium'
+                    >
+                      Team
+                    </Link>
+                    <Link
+                      href='#'
+                      className='text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium'
+                    >
+                      Projects
+                    </Link>
+                    <Link
+                      href='#'
+                      className='text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium'
+                    >
+                      Calendar
+                    </Link>
+                    <Link
+                      href='#'
+                      className='text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium'
+                    >
+                      Reports
+                    </Link>
+                  </div>
+                </div>
+              </div>
+              <div className='hidden md:block'>
+                <div className='ml-4 flex items-center md:ml-6'>
+                  <button
+                    type='button'
+                    className='rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'
+                  >
+                    <span className='sr-only'>View notifications</span>
+                    <svg
+                      className='h-6 w-6'
+                      fill='none'
+                      viewBox='0 0 24 24'
+                      strokeWidth='1.5'
+                      stroke='currentColor'
+                      aria-hidden='true'
+                    >
+                      <path
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
+                        d='M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0'
+                      />
+                    </svg>
+                  </button>
+
+                  {/* <!-- Profile dropdown --> */}
+                  <div className='relative ml-3'>
+                    <div>
+                      <button
+                        type='button'
+                        className='flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'
+                        id='user-menu-button'
+                        aria-expanded='false'
+                        aria-haspopup='true'
+                      >
+                        <span className='sr-only'>Open user menu</span>
+                        <Image
+                          src='/Virender-verma.jpg'
+                          className='h-8 w-8 rounded-full'
+                          width={80}
+                          height={80}
+                          alt='Picture of the author'
+                        />
+                        {/* <Image
+                          className='h-8 w-8 rounded-full'
+                          src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+                          width={120}
+                          height={80}
+                          alt='Profile'
+                        /> */}
+                      </button>
+                    </div>
+
+                    <div
+                      className='absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'
+                      role='menu'
+                      aria-orientation='vertical'
+                      aria-labelledby='user-menu-button'
+                      tabIndex='-1'
+                    >
+                      {/* <!-- Active: "bg-gray-100", Not Active: "" --> */}
+                      <Link
+                        href='#'
+                        className='block px-4 py-2 text-sm text-gray-700'
+                        role='menuitem'
+                        tabIndex='-1'
+                        id='user-menu-item-0'
+                      >
+                        Your Profile
+                      </Link>
+                      <Link
+                        href='#'
+                        className='block px-4 py-2 text-sm text-gray-700'
+                        role='menuitem'
+                        tabIndex='-1'
+                        id='user-menu-item-1'
+                      >
+                        Settings
+                      </Link>
+                      <Link
+                        href='#'
+                        className='block px-4 py-2 text-sm text-gray-700'
+                        role='menuitem'
+                        tabIndex='-1'
+                        id='user-menu-item-2'
+                      >
+                        Sign out
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className='-mr-2 flex md:hidden'>
+                {/* <!-- Mobile menu button --> */}
+                <button
+                  type='button'
+                  className='inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'
+                  aria-controls='mobile-menu'
+                  aria-expanded='false'
+                >
+                  <span className='sr-only'>Open main menu</span>
+                  {/* <!-- Menu open: "hidden", Menu closed: "block" --> */}
+                  <svg
+                    className='block h-6 w-6'
+                    fill='none'
+                    viewBox='0 0 24 24'
+                    strokeWidth='1.5'
+                    stroke='currentColor'
+                    aria-hidden='true'
+                  >
+                    <path
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      d='M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5'
+                    />
+                  </svg>
+                  {/* <!-- Menu open: "block", Menu closed: "hidden" --> */}
+                  <svg
+                    className='hidden h-6 w-6'
+                    fill='none'
+                    viewBox='0 0 24 24'
+                    strokeWidth='1.5'
+                    stroke='currentColor'
+                    aria-hidden='true'
+                  >
+                    <path
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      d='M6 18L18 6M6 6l12 12'
+                    />
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </div>
+
+          {/* <!-- Mobile menu, show/hide based on menu state. --> */}
+          <div className='md:hidden' id='mobile-menu'>
+            <div className='space-y-1 px-2 pb-3 pt-2 sm:px-3'>
+              {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
+              <Link
+                href='#'
+                className='bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium'
+                aria-current='page'
+              >
+                Dashboard
+              </Link>
+              <Link
+                href='#'
+                className='text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium'
+              >
+                Team
+              </Link>
+              <Link
+                href='#'
+                className='text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium'
+              >
+                Projects
+              </Link>
+              <Link
+                href='#'
+                className='text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium'
+              >
+                Calendar
+              </Link>
+              <Link
+                href='#'
+                className='text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium'
+              >
+                Reports
+              </Link>
+            </div>
+            <div className='border-t border-gray-700 pb-3 pt-4'>
+              <div className='flex items-center px-5'>
+                <div className='flex-shrink-0'>
+                  <Image
+                    className='h-10 w-10 rounded-full'
+                    width={80}
+                    height={80}
+                    src='/Virender-verma.jpg'
+                    alt='images of tailwind'
+                  />
+                </div>
+                <div className='ml-3'>
+                  <div className='text-base font-medium leading-none text-white'>
+                    Tom Cook
+                  </div>
+                  <div className='text-sm font-medium leading-none text-gray-400'>
+                    tom@example.com
+                  </div>
+                </div>
+                <button
+                  type='button'
+                  className='ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'
+                >
+                  <span className='sr-only'>View notifications</span>
+                  <svg
+                    className='h-6 w-6'
+                    fill='none'
+                    viewBox='0 0 24 24'
+                    strokeWidth='1.5'
+                    stroke='currentColor'
+                    aria-hidden='true'
+                  >
+                    <path
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      d='M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0'
+                    />
+                  </svg>
+                </button>
+              </div>
+              <div className='mt-3 space-y-1 px-2'>
+                <Link
+                  href='#'
+                  className='block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white'
+                >
+                  Your Profile
+                </Link>
+                <Link
+                  href='#'
+                  className='block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white'
+                >
+                  Settings
+                </Link>
+                <Link
+                  href='#'
+                  className='block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white'
+                >
+                  Sign out
+                </Link>
+              </div>
+            </div>
+          </div>
+        </nav>
+
+        <header className='bg-white shadow'>
+          <div className='mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8'>
+            <h1 className='text-3xl font-bold tracking-tight text-gray-900'>
+              Dashboard
+            </h1>
+          </div>
+        </header>
+        <main>
+          <div className='mx-auto max-w-7xl py-6 sm:px-6 lg:px-8'>
+            {/* <!-- Your content --> */}
+          </div>
+        </main>
       </div>
+    </>
+  );
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Page;
